66b91b6b7f1177f35dd6a0951e45515726ddfe77
dy4wd <teylor.neyll@gmail.com>
2025-02-18 14:05:00 +0400

    sets the target folder

--- minimanga/main.py
+++ minimanga/main.py
@@ -1,11 +1,21 @@
+import sys
 import asyncio
 
+import utils
+
 from console import get_command_line_arguments
+from exceptions import SpecifiedPathNotFolder
 
 
 async def main():
     cli_args = get_command_line_arguments()
-    print(cli_args)
+
+    try:
+        target_folder = utils.check_path(cli_args.path)
+    except SpecifiedPathNotFolder:
+        print("The specified path is not a folder.", file=sys.stderr)
+        sys.exit(1)
+    print(target_folder)
 
 
 if __name__ == "__main__":
