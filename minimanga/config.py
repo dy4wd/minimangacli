bc2d6027f0940100f97dbef0f514961a2fc86407
dy4wd <teylor.neyll@gmail.com>
2025-02-26 21:13:10 +0400

    tests the path generator for saving

--- minimanga/config.py
+++ minimanga/config.py
@@ -2,3 +2,4 @@ QUALITY = 75
 IMAGES_SUFFIXES = (".jpg", ".jpeg", ".png")
 SUFFIX_FOLDER_TO_SAVE = "_mini"
 MAX_SIZE_WEBP = 16383
+WEBP_SUFFIX = ".webp"
--- minimanga/image_formatter.py
+++ minimanga/image_formatter.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 from dataclasses import dataclass
+from typing import Callable
 
+from . import config
 from .utils import Images
 
 
 @dataclass(frozen=True, slots=True)
 class DataForFormatter:
-    images: Images
-    to_save: Path
+    target_images: Images
+    manga_name: str
     image_quality: int
 
 
@@ -15,5 +17,15 @@ def format_images(data: DataForFormatter):
     pass
 
 
-def _generate_path_to_save(folder_to_save: Path, image: Path):
-    pass
+def _generate_path_to_save(manga_name: str, image: Path) -> Path:
+    image = _change_suffix(image)
+    root_dir, tail = str(image).split(manga_name)
+    new_manga_name = f"{manga_name}{config.SUFFIX_FOLDER_TO_SAVE}"
+    path_to_save = Path(root_dir, new_manga_name, tail[1:])
+    return path_to_save
+
+
+_change_suffix: Callable[[Path], Path] = lambda image: image.with_suffix(config.WEBP_SUFFIX)
+
+
+
