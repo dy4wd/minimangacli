0ffe4911ce901102d162a955b2cd6cc1117715cc
dy4wd <teylor.neyll@gmail.com>
2025-04-09 17:31:00 +0400

    transfers the trash cleanup to the ArchiveHandler class

--- minimanga/handlers/archive_handler.py
+++ minimanga/handlers/archive_handler.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 
 from enum import Enum
 from pathlib import Path
@@ -6,7 +7,7 @@ from pathlib import Path
 from zipfile import ZipFile
 from rarfile import RarFile
 
-from .. import search, config
+from .. import search
 
 
 class ArchiveType(str, Enum):
@@ -19,37 +20,39 @@ class ArchiveType(str, Enum):
 class ArchiveHandler:
     def __init__(self, folder: Path):
         self._folder = folder
-        self._dist: Path | None = None
         self._suffixes = ("zip", "cbz", "rar", "cbr")
-        self._archives = search.find_all(self._folder, self._suffixes)
+        self._trash = []
 
     def unpack(self):
-        for archive in self._archives:
+        archives = search.find_all(self._folder, self._suffixes)
+        for archive in archives:
             sys.stdout.write(f"Unpacking: {archive.name}\n")
             suffix = archive.suffix[1:]
-            self._dist = self._set_dist_folder(archive)
+            dist = self._set_dist_folder(archive)
             match suffix:
                 case ArchiveType.ZIP | ArchiveType.CBZ:
-                    self._extract_zip(archive)
+                    self._extract_zip(archive, dist)
                 case ArchiveType.RAR | ArchiveType.CBR:
-                    self._extract_rar(archive)
+                    self._extract_rar(archive, dist)
                 case _:
                     sys.stderr.write(f'"{suffix}" is an unknown archive type.\n')
                     exit(1)
+        self._clean()
 
     def _set_dist_folder(self, archive: Path) -> Path:
         return Path(archive.parent, archive.stem)
 
-    def _extract_zip(self, archive: Path):
+    def _extract_zip(self, archive: Path, dist: Path):
         with ZipFile(archive) as zipfile_:
-            zipfile_.extractall(path=self._dist)
-            self._in_trash(str(self._dist))
+            zipfile_.extractall(path=dist)
+            self._trash.append(dist)
 
-    def _extract_rar(self, archive: Path):
+    def _extract_rar(self, archive: Path, dist: Path):
         with RarFile(archive) as rarfile_:
-            rarfile_.extractall(path=self._dist)
-            self._in_trash(str(self._dist))
+            rarfile_.extractall(path=dist)
+            self._trash.append(dist)
 
-    def _in_trash(self, path: str):
-        with open(config.TRASH_FILE, "a") as file:
-            file.write(f"{path}\n")
+    def _clean(self):
+        sys.stdout.write("\nDeleting folders after unpacking.")
+        for item in self._trash:
+            shutil.rmtree(item)
