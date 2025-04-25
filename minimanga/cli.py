e9e44720ac0c834a6c1d14ca6b752d5352d5a227
dy4wd <teylor.neyll@gmail.com>
2025-04-25 22:14:08 +0400

    add new cli argument

--- minimanga/cli.py
+++ minimanga/cli.py
@@ -9,6 +9,7 @@ from minimanga import config
 @dataclass(frozen=True, slots=True)
 class CLIArguments:
     path: Path
+    format_: str
     quality: int
     is_archives: bool
 
@@ -17,6 +18,7 @@ def get_arguments() -> CLIArguments:
     cli_args = _create_command_line_argument_parser()
     return CLIArguments(
         path=cli_args.path.absolute(),
+        format_=cli_args.format,
         quality=cli_args.quality,
         is_archives=cli_args.is_archives,
     )
@@ -30,5 +32,6 @@ def _create_command_line_argument_parser() -> argparse.Namespace:
 
 def _add_arguments_to_parser(parser: argparse.ArgumentParser):
     parser.add_argument('path', type=Path)
+    parser.add_argument('-f', '--format', type=str, choices=['webp', 'avif', 'jpeg'], default='webp')
     parser.add_argument('-q', '--quality', type=int, default=config.QUALITY)
     parser.add_argument('-a', dest='is_archives', action='store_true')
